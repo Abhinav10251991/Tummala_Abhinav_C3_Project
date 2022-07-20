6d50b29f1b761a23d63f0be18836e8e8ee3b2216
Abhinav10251991 <mail2abhinavtummala@gmail.com>
2022-07-20 13:45:48 +0100

    Part 3: Solution

--- src/main/java/Item.java
+++ src/main/java/Item.java
@@ -7,9 +7,9 @@ public class Item {
         this.price = price;
     }
     //Implementing the new method to test TDD//
-//    public int getPrice() {
-//    return price;
-//    }
+    public int getPrice() {
+    return price;
+    }
 
     public String getName() {
         return name;
